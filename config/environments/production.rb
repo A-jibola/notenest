529ab0c943a788ab360ca0456f8c119ade34d75b
A-jibola <okesolajibola@gmail.com>
2026-01-08 23:12:00 +0100

    Update SMTP port configuration to use environment variable for flexibility

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -73,7 +73,7 @@ Rails.application.configure do
     password: ENV["RESEND_KEY"],
     domain: ENV["WEBSITE_DOMAIN"],
     address: "smtp.resend.com",
-    port: 587,
+    port: ENV["SMPT_PORT"],
     authentication: :plain,
     enable_starttls_auto: true
   }
