f3a98f783158e73dadefee025e37cbe740def64e
A-jibola <okesolajibola@gmail.com>
2026-01-09 22:33:06 +0100

    Enable mailer deliveries and adjust caching settings for immediate template updates

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,6 +61,14 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = true
 
+  
+  # perform mailer deliveries
+  config.action_mailer.perform_deliveries = true
+
+  # Make template changes take effect immediately.
+  config.action_mailer.perform_caching = false
+
+  
   # Set host to be used by links generated in mailer templates.
 
   config.action_mailer.delivery_method = :smtp
